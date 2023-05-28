50a50b082752ec45f2314bc8e0fe5e0194f6f11e
Andrii Shchur <andriis@profisealabs.com>
2023-05-28 22:44:36 +0300

    added fake news detector

---
+++ detector.py
@@ -0,0 +1,43 @@
+import re
+import numpy as np
+
+from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+from models import Article
+
+
+class Detector:
+    def __init__(self, content: Article):
+        self._content = content
+        self.one_hot_n = 5000
+        self.pad_length = 40
+        self._model = load_model('model')
+        self.stopwords = set(stopwords.words('english'))
+        self.lemmatizer = WordNetLemmatizer()
+
+    def _text_to_input(self, text: str):
+        """
+        Converts article contents to # TODO:
+        :param text: article contents
+        :return: text converted to
+        """
+        text = text.lower()
+        text = re.sub('[^\s\w]|[\d]', '', text)
+        text = ' '.join(word for word in text.split() if word not in self.stopwords)
+        text = ' '.join(self.lemmatizer.lemmatize(word) for word in text.split())
+
+        one_hot_text = one_hot(text, self.one_hot_n)
+        res = pad_sequences([one_hot_text], padding='pre', maxlen=self.pad_length)
+        return np.array(res)
+
+    def detect(self) -> float:
+        article_text = self._content.title + ' ' + self._content.text
+        X = self._text_to_input(article_text)
+        prediction = self._model.predict(X)
+
+        return float(prediction[0][0])
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import nltk
+
 from fastapi import FastAPI
+
+from detector import Detector
+from models import Article
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.on_event('startup')
+async def startup():
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+
+
+@app.post('/detect')
+async def detect(content: Article):
+    detector = Detector(content)
+    detection = detector.detect()
+
+    return {'detection': detection}
