63752080c886915a138178934876b089e13a7ea1
Andrii Shchur <andriis@profisealabs.com>
2023-05-29 21:59:21 +0300

    pretty much working frontend

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import nltk
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from detector import Detector
 from models import Article
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 
 @app.on_event('startup')
